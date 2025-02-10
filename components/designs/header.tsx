0e8901c0c4216950a3d7cc30fc71d0a3269b611f
hemanth18 <hg510443@gmail.com>
2025-02-10 23:37:57 +0530

    Improved responsiveness

--- components/designs/header.tsx
+++ components/designs/header.tsx
@@ -4,15 +4,15 @@ export function Header() {
   const sections: string[] = ["Home", "About", "Skills", "Projects", "Education", "Contact"];
 
   return (
-    <header className="fixed top-0 z-50 w-full p-4">
-      <div className="backdrop-blur-lg text-gray-300 text-lg flex justify-center items-center rounded-full px-6 py-3 w-full max-w-screen-xl mx-auto">
-        {/* Flex container for the navigation links */}
-        <div className="inline-flex space-x-8 flex-wrap justify-center md:justify-center md:space-x-12">
+    <header className="fixed top-0 z-50 w-full p-2">
+      <div className="backdrop-blur-lg text-gray-300 text-lg flex justify-center items-center rounded-full px-6 w-full max-w-screen-xl mx-auto">
+        {/* Responsive flex container */}
+        <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:space-x-12 sm:max-w-sm md:max-w-full">
           {sections.map((section: string) => (
             <Link
               key={section}
               href={`#${section.toLowerCase()}`}
-              className="text-gray-400 cursor-pointer hover:underline hover:text-white hover:rounded-full px-3 py-1 transition-all duration-300 text-center"
+              className="text-gray-400 cursor-pointer hover:underline hover:text-white hover:rounded-full px-3 py-1 transition-all duration-300 text-center text-sm sm:text-base"
             >
               {section}
             </Link>
