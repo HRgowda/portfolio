dfa277027df41e3f840da7ff846b5e869e9f8d0b
hemanth18 <hg510443@gmail.com>
2025-06-06 14:11:44 +0530

    Improved responsiveness

--- components/designs/header.tsx
+++ components/designs/header.tsx
@@ -4,10 +4,9 @@ export function Header() {
   const sections: string[] = ["Home", "About", "Experience", "Skills", "Projects", "Achievements", "Education", "Contact"];
 
   return (
-    <header className="fixed top-0 z-50 w-full p-2">
-      <div className="backdrop-blur-lg text-gray-300 text-lg flex justify-center items-center rounded-full px-6 w-full max-w-screen-xl mx-auto">
-        {/* Responsive flex container */}
-        <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:space-x-12 sm:max-w-sm md:max-w-full">
+    <header className="fixed top-0 z-50 w-full py-2">
+      <div className="backdrop-blur-lg text-gray-300 text-lg flex justify-center items-center rounded-full max-w-lg max-w-screen-xl mx-auto">
+        <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:space-x-12 sm:max-w-lg md:max-w-full">
           {sections.map((section: string) => (
             <Link
               key={section}
